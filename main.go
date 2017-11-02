da8078a0ab287bf9f5e8cc435028ed6386cd2d2c
Sergey Yazan <sergey.yazan@gmail.com>
2017-11-02 18:41:19 +0300

    adds beta grpc server

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
 
+	"github.com/roflomglol/estimator/maps"
+
+	pb "github.com/roflomglol/estimator/estimation"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":3001"
+)
+
+type server struct{}
+
+func (s *server) TimeAndDistanceBetweenPoints(ctx context.Context, in *pb.PointsInfo) (*pb.DirectionsInfo, error) {
+	start := maps.Point{Lat: in.Lat1, Long: in.Long1}
+	finish := maps.Point{Lat: in.Lat2, Long: in.Long2}
+	time, distance := maps.CalculateTimeAndDistance(&start, &finish)
+
+	return &pb.DirectionsInfo{Time: int32(time), Distance: int32(distance)}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterEstimationServer(s, &server{})
+	s.Serve(lis)
 }
